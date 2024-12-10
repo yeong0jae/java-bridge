73ced8c19655b8b96554c7f5f24fed9870b36152
Yeongjae Kim <sees77770@gmail.com>
2024-12-10 19:59:09 +0900

    feat: 다리 건너기 게임 컨트롤러 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        BridgeGameController bridgeGameController = new BridgeGameController(inputView, outputView);
+        bridgeGameController.play();
     }
 }
